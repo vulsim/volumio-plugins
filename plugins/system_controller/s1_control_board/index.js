169ec7912202f8748a8330e2b4d263fc01f3d30b
Igor Mikheiko <vulsim@mail.ru>
2021-06-27 01:03:57 +0300

    updating plugin s1_control_board version 1.0.0

--- plugins/system_controller/s1_control_board/index.js
+++ plugins/system_controller/s1_control_board/index.js
@@ -6,6 +6,8 @@ var config = new (require('v-conf'))();
 var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
 
+const SerialPort = require("serialport");
+const serialPort = new SerialPort("/dev/ttyUSB0", { baudRate: 57600, autoOpen: true });
 
 module.exports = s1ControlBoard;
 function s1ControlBoard(context) {
@@ -35,7 +37,7 @@ s1ControlBoard.prototype.onStart = function() {
 	var defer=libQ.defer();
 
 
-	// Once the Plugin has successfull started resolve the promise
+	serialPort.write("SYS_ON\n");
 	defer.resolve();
 
     return defer.promise;
@@ -45,7 +47,7 @@ s1ControlBoard.prototype.onStop = function() {
     var self = this;
     var defer=libQ.defer();
 
-    // Once the Plugin has successfull stopped resolve the promise
+    serialPort.write("SYS_OFF\n");
     defer.resolve();
 
     return libQ.resolve();
@@ -53,7 +55,7 @@ s1ControlBoard.prototype.onStop = function() {
 
 s1ControlBoard.prototype.onRestart = function() {
     var self = this;
-    // Optional, use if you need it
+    serialPort.write("SYS_RESTART\n");
 };
 
 
@@ -87,181 +89,13 @@ s1ControlBoard.prototype.getConfigurationFiles = function() {
 }
 
 s1ControlBoard.prototype.setUIConfig = function(data) {
-	var self = this;
-	//Perform your installation tasks here
+
 };
 
 s1ControlBoard.prototype.getConf = function(varName) {
-	var self = this;
-	//Perform your installation tasks here
+
 };
 
 s1ControlBoard.prototype.setConf = function(varName, varValue) {
-	var self = this;
-	//Perform your installation tasks here
-};
 
-
-
-// Playback Controls ---------------------------------------------------------------------------------------
-// If your plugin is not a music_sevice don't use this part and delete it
-
-
-s1ControlBoard.prototype.addToBrowseSources = function () {
-
-	// Use this function to add your music service plugin to music sources
-    //var data = {name: 'Spotify', uri: 'spotify',plugin_type:'music_service',plugin_name:'spop'};
-    this.commandRouter.volumioAddToBrowseSources(data);
-};
-
-s1ControlBoard.prototype.handleBrowseUri = function (curUri) {
-    var self = this;
-
-    //self.commandRouter.logger.info(curUri);
-    var response;
-
-
-    return response;
-};
-
-
-
-// Define a method to clear, add, and play an array of tracks
-s1ControlBoard.prototype.clearAddPlayTrack = function(track) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 's1ControlBoard::clearAddPlayTrack');
-
-	self.commandRouter.logger.info(JSON.stringify(track));
-
-	return self.sendSpopCommand('uplay', [track.uri]);
-};
-
-s1ControlBoard.prototype.seek = function (timepos) {
-    this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 's1ControlBoard::seek to ' + timepos);
-
-    return this.sendSpopCommand('seek '+timepos, []);
-};
-
-// Stop
-s1ControlBoard.prototype.stop = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 's1ControlBoard::stop');
-
-
-};
-
-// Spop pause
-s1ControlBoard.prototype.pause = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 's1ControlBoard::pause');
-
-
-};
-
-// Get state
-s1ControlBoard.prototype.getState = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 's1ControlBoard::getState');
-
-
-};
-
-//Parse state
-s1ControlBoard.prototype.parseState = function(sState) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 's1ControlBoard::parseState');
-
-	//Use this method to parse the state and eventually send it with the following function
-};
-
-// Announce updated State
-s1ControlBoard.prototype.pushState = function(state) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 's1ControlBoard::pushState');
-
-	return self.commandRouter.servicePushState(state, self.servicename);
-};
-
-
-s1ControlBoard.prototype.explodeUri = function(uri) {
-	var self = this;
-	var defer=libQ.defer();
-
-	// Mandatory: retrieve all info for a given URI
-
-	return defer.promise;
-};
-
-s1ControlBoard.prototype.getAlbumArt = function (data, path) {
-
-	var artist, album;
-
-	if (data != undefined && data.path != undefined) {
-		path = data.path;
-	}
-
-	var web;
-
-	if (data != undefined && data.artist != undefined) {
-		artist = data.artist;
-		if (data.album != undefined)
-			album = data.album;
-		else album = data.artist;
-
-		web = '?web=' + nodetools.urlEncode(artist) + '/' + nodetools.urlEncode(album) + '/large'
-	}
-
-	var url = '/albumart';
-
-	if (web != undefined)
-		url = url + web;
-
-	if (web != undefined && path != undefined)
-		url = url + '&';
-	else if (path != undefined)
-		url = url + '?';
-
-	if (path != undefined)
-		url = url + 'path=' + nodetools.urlEncode(path);
-
-	return url;
-};
-
-
-
-
-
-s1ControlBoard.prototype.search = function (query) {
-	var self=this;
-	var defer=libQ.defer();
-
-	// Mandatory, search. You can divide the search in sections using following functions
-
-	return defer.promise;
-};
-
-s1ControlBoard.prototype._searchArtists = function (results) {
-
-};
-
-s1ControlBoard.prototype._searchAlbums = function (results) {
-
-};
-
-s1ControlBoard.prototype._searchPlaylists = function (results) {
-
-
-};
-
-s1ControlBoard.prototype._searchTracks = function (results) {
-
-};
-
-s1ControlBoard.prototype.goto=function(data){
-    var self=this
-    var defer=libQ.defer()
-
-// Handle go to artist and go to album function
-
-     return defer.promise;
 };
